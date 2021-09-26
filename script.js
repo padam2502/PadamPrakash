5431260c4481eebc6f7abec60118892741982a12
padam2502 <padamprakash2014@gmail.com>
2021-09-26 13:10:25 +0530

    commiting

---
+++ script.js
@@ -0,0 +1,33 @@
+var tabButtons=document.querySelectorAll(".tabContainer .buttonContainer button");
+var tabPanels=document.querySelectorAll(".tabContainer  .tabPanel");
+
+function showPanel(panelIndex,colorCode) {
+    tabButtons.forEach(function(node){
+        node.style.backgroundColor="";
+        node.style.color="";
+    });
+    tabButtons[panelIndex].style.backgroundColor="black";
+    tabButtons[panelIndex].style.color="white";
+    tabPanels.forEach(function(node){
+        node.style.display="none";
+    });
+    tabPanels[panelIndex].style.display="block";
+    tabPanels[panelIndex].style.backgroundColor=colorCode;
+}
+showPanel(0,'#f44336');
+function display_c(){
+var refresh=1000; // Refresh rate in milli seconds
+mytime=setTimeout('display_ct()',refresh)
+}
+
+function display_ct() {
+var x = new Date()
+document.getElementById('ct').innerHTML = x;
+display_c();
+ }
+ //function autoResizeDiv()
+   //     {
+     //       document.getElementById('main').style.height = window.innerHeight +'px';
+       // }
+        //window.onresize = autoResizeDiv;
+        //autoResizeDiv();
